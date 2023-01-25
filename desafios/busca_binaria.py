d26ee1ff91a48ec520e42f2b2f75465c521c76ca
UdsonWillams <udson.willams@gmail.com>
2023-01-24 21:35:04 -0300

    adicionando questao de busca binaria.

---
+++ desafios/busca_binaria.py
@@ -0,0 +1,5 @@
+"""
+Utilizamos um exemplo bem simples agora, mas imagine ter que desenvolver uma feature
+de busca de alunos da Alura para que seja possível realizar a autenticação
+no site ou app. Qual solução você usaria?
+"""
