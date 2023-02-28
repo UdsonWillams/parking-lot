a8be64ec2133dfe81a684a3afb8b8a922287c6cc
UdsonWillams <udson.willams@gmail.com>
2023-02-27 22:26:22 -0300

    atualização continua

--- desafios/busca_binaria.py
+++ desafios/busca_binaria.py
@@ -3,3 +3,4 @@ Utilizamos um exemplo bem simples agora, mas imagine ter que desenvolver uma fea
 de busca de alunos da Alura para que seja possível realizar a autenticação
 no site ou app. Qual solução você usaria?
 """
+# resolução https://www.alura.com.br/artigos/busca-binaria-aprenda-implementar-python"
