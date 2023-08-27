dd82bfcef987f985bf9d0c5f2d244305001bcddc
UdsonWillams <udson.willams@gmail.com>
2023-08-27 01:07:29 -0300

    exceptions retornadas pela classe repository

---
+++ desafios/url_shortener/api/db/repository/exceptions.py
@@ -0,0 +1,43 @@
+from typing import Any, Dict
+
+from fastapi import HTTPException
+
+
+class UrlNotFoundException(HTTPException):
+    def __init__(
+        self,
+        status_code: int,
+        detail: Any = None,
+        headers: Dict[str, str] | None = None,
+    ) -> None:
+        super().__init__(status_code, detail, headers)
+
+
+class CreateUrlException(HTTPException):
+    def __init__(
+        self,
+        status_code: int,
+        detail: Any = None,
+        headers: Dict[str, str] | None = None,
+    ) -> None:
+        super().__init__(status_code, detail, headers)
+
+
+class DeleteUrlException(HTTPException):
+    def __init__(
+        self,
+        status_code: int,
+        detail: Any = None,
+        headers: Dict[str, str] | None = None,
+    ) -> None:
+        super().__init__(status_code, detail, headers)
+
+
+class ExpirationTimeException(HTTPException):
+    def __init__(
+        self,
+        status_code: int,
+        detail: Any = None,
+        headers: Dict[str, str] | None = None,
+    ) -> None:
+        super().__init__(status_code, detail, headers)
