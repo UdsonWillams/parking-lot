9e1ad984ae9f275e338c6f3a280e3f1f11e7e662
UdsonWillams <udson.willams@gmail.com>
2024-05-16 18:36:10 -0300

    melhorias

--- parking lot/batida_de_ponto.py
+++ parking lot/batida_de_ponto.py
@@ -2,25 +2,29 @@
 # saida, e o script responder a correta de sair do job.
 # com base que o horario são 8 horas diarias.
 
-# TODO
-from datetime import datetime
+from datetime import (
+    datetime,
+    timedelta,
+)
 
-entrada1 = datetime.strptime("08-45", "%H-%M")
-saida1 = datetime.strptime("14-14", "%H-%M")
+hours_per_day = 8
 
-entrada2 = datetime.strptime("14-50", "%H-%M")
+if __name__ == "__main__":
+    # markeup hours
+    work_entrance = "11:37"
+    pre_lunch = "12:23"
+    post_lunch = "13:50"
 
-final = saida1 - entrada1
+    today = datetime.now().date().strftime("%m/%d/%Y")
+    work_entrance = datetime.strptime(f"{today} {work_entrance}", "%m/%d/%Y %H:%M")
+    final_time = work_entrance + timedelta(hours=hours_per_day)
 
+    pre_lunch_time = datetime.strptime(f"{today} {pre_lunch}", "%m/%d/%Y %H:%M")
+    post_lunch_time = datetime.strptime(f"{today} {post_lunch}", "%m/%d/%Y %H:%M")
 
-cont_hr = 0
-cont_min = 0
-while final.days < 8:
-    if cont_min > 60:
-        cont_min = 0
-        cont_hr += 1
-    saida = datetime.strptime(f"{cont_hr}-{cont_min}", "%H-%M")
-    entrada2 + saida
-    final = final + saida1
+    diference = post_lunch_time - timedelta(
+        hours=pre_lunch_time.hour, minutes=pre_lunch_time.minute
+    )
+    final_time = final_time + timedelta(hours=diference.hour, minutes=diference.minute)
 
-print(final)
+    print(f"A hora de sair são as: {final_time.time()}")
